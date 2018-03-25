9e51aca3816b69b74d5c8691e4c77ebe6565b30a
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-03-25 16:24:09 +0200

    Try to encode in known format

--- lora_single_chan_gateway/gateway.py
+++ lora_single_chan_gateway/gateway.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import base64
 import logging
 
 from board_config import LoraBoardDraguino
@@ -9,6 +10,25 @@ import RPi.GPIO as GPIO
 logging.basicConfig(level=logging.DEBUG)
 
 
+def construct_semtec_udp(board, payload):
+    return {
+        "rxpk":
+            {
+                "time": payload['datetime'].isoformat(),
+                "freq": board.frequency / 1000 / 1000,
+                "stat": 1 if payload['crc'] is True else -1,
+                "modu": "LORA",
+                "datr": "SF7BW125",  # TODO: configurable
+                "codr": "4/5",
+                "rssi": payload['pkt_rssi'],
+                "lsnr": payload['pkt_ssr'],
+                "size": len(payload['payload']),
+                "data": base64.standard_b64encode(payload['payload'])
+            }
+    }
+
+
+
 if __name__ == "__main__":
     with LoraBoardDraguino(433300000, 7) as board:
         logging.info("Listening at SF{} on {} MHz".format(board.sf, board.frequency/1000000))
@@ -16,4 +36,5 @@ if __name__ == "__main__":
         while True:
             if GPIO.input(board._pin_dio0) == 1:
                 payload = board.receive_package()
-                logging.info("Received: {}".format(payload))
+                logging.info(construct_semtec_udp(board, payload))
+                #logging.info("Received: {}".format(payload))
