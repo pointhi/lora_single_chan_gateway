801e22905b5b488cfce05d26730ec937097d6abd
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-06-18 19:08:50 +0200

    Add sqllite logger

--- lora_single_chan_gateway/gateway.py
+++ lora_single_chan_gateway/gateway.py
@@ -5,6 +5,7 @@ import json
 import logging
 from random import randint
 import socket
+import sqlite3
 
 from board_config import LoraBoardDraguino
 import RPi.GPIO as GPIO
@@ -15,6 +16,51 @@ logging.basicConfig(level=logging.DEBUG)
 
 GATEWAY_HOST = "router.eu.thethings.network"
 GATEWAY_PORT = 1700
+
+
+class LoRaPacketsDB(object):
+    def __init__(self):
+        self.conn = sqlite3.connect('gateway_packages.db')  # we only need the db at runtime
+        self.c = self.conn.cursor()
+
+        self._init_db()
+
+    def _init_db(self):
+        create_table = """CREATE TABLE IF NOT EXISTS `PACKETS` (
+            `ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            `DATETIME` TEXT NOT NULL,
+            `DATA` BLOB,
+            `FREQUENCY` NUMERIC,
+            `IS_CRC_OK` INTEGER,
+            `PKT_SNR` NUMERIC,
+            `PKT_RSSI` NUMERIC,
+            `RSSI` NUMERIC
+        )"""
+        self.conn.execute(create_table)
+
+    def log_pkg(self, board, packet):
+        query = """INSERT INTO `PACKETS` (
+            `DATETIME`,
+            `DATA`,
+            `FREQUENCY`,
+            `IS_CRC_OK`,
+            `PKT_SNR`,
+            `PKT_RSSI`,
+            `RSSI`
+        ) VALUES (?, ?, ?, ?)
+        """
+        try:
+            self.c.execute(query, (payload['datetime'].isoformat(),
+                                   payload.get('payload'),
+                                   board.frequency,
+                                   payload.get('crc'),
+                                   payload.get('pkt_snr'),
+                                   payload.get('pkt_rssi'),
+                                   payload.get('rssi')))
+            self.conn.commit()
+        except sqlite3.IntegrityError as e:
+            logging.exception("SQL ERROR: ", e)
+
 
 def construct_semtec_udp(board, payload):
     # https://github.com/Lora-net/packet_forwarder/blob/d0226eae6e7b6bbaec6117d0d2372bf17819c438/PROTOCOL.TXT#L99
@@ -39,13 +85,15 @@ def construct_semtec_udp(board, payload):
             }
     }
 
-    frame.extend(map(ord,json.dumps(data)))
+    frame.extend(map(ord, json.dumps(data)))
 
     return bytes(frame)
 
 
 if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+
+    db = LoRaPacketsDB()
 
     with LoraBoardDraguino(433300000, 7) as board:
         board.set_mode_rx()
@@ -55,8 +103,11 @@ if __name__ == "__main__":
             if GPIO.input(board._pin_dio0) == 1:
                 payload = board.receive_package()
 
-                #semtec_udp = construct_semtec_udp(board, payload)
-                #sock.sendto(semtec_udp, (GATEWAY_HOST, GATEWAY_PORT))
-                #logging.info(semtec_udp)
+                # semtec_udp = construct_semtec_udp(board, payload)
+                # sock.sendto(semtec_udp, (GATEWAY_HOST, GATEWAY_PORT))
+                # logging.info(semtec_udp)
 
-                logging.info("Received: {}".format(payload))
+                # logging.info("Received: {}".format(payload))
+                logging.info("Received at {}: {} = {}".format(payload['datetime'].isoformat(), payload['payload'].hex(), payload['payload'][4:]))
+
+                db.log_pkg(board, payload)
